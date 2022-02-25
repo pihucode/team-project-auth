2d44d09610749b9f3cc62a1c2fca79e00451ed2b
Pinxuan Huang <pinxhuang@gmail.com>
2022-02-25 18:03:30 -0500

    Initial commit

---
+++ src/main/java/com/example/teamprojectauth/TeamProjectAuthApplication.java
@@ -0,0 +1,13 @@
+package com.example.teamprojectauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamProjectAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamProjectAuthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/teamprojectauth/TeamProjectAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.teamprojectauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamProjectAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
